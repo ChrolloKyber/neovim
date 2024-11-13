cf151e6aabfab7adffc62c579e954c596c1fb20e
Chrollo <89532437+ChrolloKryber@users.noreply.github.com>
2024-11-13 08:59:20 +0530

    fix esc behavior

--- lua/chrollo/maps.lua
+++ lua/chrollo/maps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set('n', '<leader>pv', '<CMD>Ex<CR>', { silent = true, desc = "Launch Netrw" })
-vim.keymap.set('n', '<ESC>', 'set nohlsearch', { silent = true, desc = "Clear search highlight" })
+vim.keymap.set('n', '<ESC>', '<CMD>set nohlsearch<CR>', { silent = true, desc = "Clear search highlight" })
 vim.keymap.set('n', '<C-`>', 'split term://zsh', { silent = true, desc = "Launch terminal at bottom" })
