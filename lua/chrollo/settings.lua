5cdfb5e4d9fc9b4ad4e84adfd00be9680a58bb3d
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-13 11:30:42 +0530

    highlight on yank and command height
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/settings.lua
+++ lua/chrollo/settings.lua
@@ -3,7 +3,6 @@ vim.o.number = true;
 vim.o.relativenumber = true;
 vim.o.scrolloff = 10;
 vim.o.swapfile = false;
-vim.o.cmdheight = 0;
 vim.o.expandtab = true;
 vim.o.tabstop = 2;
 vim.o.softtabstop = 2;
@@ -20,3 +19,25 @@ vim.o.smartcase = true;
 vim.o.splitbelow = true;
 vim.o.splitright = true;
 vim.o.cursorline = true;
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100 }
+  end,
+})
+
+-- Toggle command line visibility
+vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function()
+        vim.o.cmdheight = 0
+    end,
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+        vim.o.cmdheight = 1
+    end,
+})
