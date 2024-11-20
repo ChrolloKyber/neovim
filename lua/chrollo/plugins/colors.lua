a5612be0d7e1e32b88ea5b0d53ce7a4d2a0d1ba8
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-20 15:21:30 +0530

    make SignColumn same as bg color
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/colors.lua
+++ lua/chrollo/plugins/colors.lua
@@ -1,10 +1,13 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		require("gruvbox").setup({
-			contrast = "hard",
-		})
-		vim.cmd.colorscheme("gruvbox")
-	end,
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      contrast = "hard",
+      overrides = {
+        SignColumn = { bg = "#1E2122" }
+      }
+    })
+    vim.cmd.colorscheme("gruvbox")
+  end,
 }
