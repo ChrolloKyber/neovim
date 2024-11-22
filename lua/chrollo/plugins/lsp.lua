fad37b11e8cb29477eda9c3641622b7984ec3241
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-22 19:52:20 +0530

    rework lsp and completion config
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/lsp.lua
+++ lua/chrollo/plugins/lsp.lua
@@ -1,160 +1,54 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/nvim-cmp",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-      "saadparwaiz1/cmp_luasnip",
-      "j-hui/fidget.nvim",
-      "numToStr/Comment.nvim",
-      {
-        "SmiteshP/nvim-navbuddy",
-        dependencies = {
-          "SmiteshP/nvim-navic",
-          "MunifTanjim/nui.nvim",
-        },
-        opts = { lsp = { auto_attach = true } },
-      },
-    },
-
+    "williamboman/mason.nvim",
+    lazy = false,
     config = function()
-      require("Comment").setup()
-      local cmp = require("cmp")
-      local cmp_lsp = require("cmp_nvim_lsp")
-      local capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        cmp_lsp.default_capabilities()
-      )
-
-      require("fidget").setup({
-        notification = {
-          window = { winblend = 0 },
-        },
-      })
       require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "pyright",
-          "terraformls",
-        },
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({
-              capabilities = capabilities,
-            })
-          end,
-
-          ["lua_ls"] = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-              capabilities = capabilities,
-              settings = {
-                Lua = {
-                  diagnostics = {
-                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                  },
-                },
-              },
-            })
-          end,
-
-          ["terraformls"] = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.terraformls.setup({
-              on_init = function(client, _)
-                client.server_capabilities.semanticTokensProvider = nil -- turn off semantic tokens
-              end,
-            })
-          end
-        },
-      })
-
-      local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-
-      vim.diagnostic.config({
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
-      })
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-          map('gD', vim.lsp.buf.declaration, "Goto Definitions")
-          map('gr', vim.lsp.buf.references, "Goto References")
-          map('gd', vim.lsp.buf.definition, "Goto Type Definitions")
-          map('<leader>cr', vim.lsp.buf.rename, "Rename")
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-        end
-      })
     end,
   },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+      local lspconfig = require("lspconfig")
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim", "it", "describe", "before_each", "after_each" },
+            },
+          },
+        },
+      })
+      lspconfig.terraformls.setup({
+        on_init = function(client, _)
+          client.server_capabilities.semanticTokensProvider = nil -- turn off semantic tokens
+        end,
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "LSP: Go to Definition" })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP: Go to References" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Actions" })
+      vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "LSP: Rename" })
+    end,
+  },
+  {
+    "RRethy/vim-illuminate",
+  },
   {
     "stevearc/conform.nvim",
     config = function()
@@ -173,30 +67,18 @@ return {
       })
     end,
   },
-
   {
-    "RRethy/vim-illuminate",
-  },
-
-  {
-    "ckipp01/nvim-jenkinsfile-linter",
-    requires = { "nvim-lua/plenary.nvim" },
+    "numToStr/Comment.nvim",
     config = function()
-      -- Create an autocommand group
-      vim.api.nvim_create_augroup("JenkinsfileLinterMappings", { clear = true })
-
-      -- Set up an autocommand to map the key only for Jenkinsfile
-      vim.api.nvim_create_autocmd("BufRead", {
-        pattern = "Jenkinsfile",
-        callback = function()
-          vim.keymap.set(
-            "n",
-            "<F5>",
-            '<CMD>lua require("jenkinsfile_linter").validate()<CR>',
-            { silent = true, desc = "Start jenkinsfile linter", buffer = 0 }
-          )
-        end,
-      })
-    end,
+      require("Comment").setup()
+    end
   },
+  {
+    "j-hui/fidget.nvim",
+    config = function()
+      require("fidget").setup({
+        notification = { window = { winblend = 0 }, },
+      })
+    end
+  }
 }
