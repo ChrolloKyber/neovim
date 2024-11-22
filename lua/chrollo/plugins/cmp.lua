d5671fe8e5ad08597669d83343e98217f137beab
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-22 20:42:55 +0530

    Select
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/cmp.lua
+++ lua/chrollo/plugins/cmp.lua
@@ -18,8 +18,9 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+      require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
         snippet = {
           expand = function(args)
