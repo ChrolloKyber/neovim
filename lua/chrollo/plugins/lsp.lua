dc0b16cfe5d8ef20b4b049fb69dcb53565ac3232
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-24 00:45:18 +0530

    lsp keymap
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/lsp.lua
+++ lua/chrollo/plugins/lsp.lua
@@ -73,7 +73,7 @@ return {
           vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP: Hover" })
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP: Go to Definition" })
           vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "LSP: Go to Declaration" })
-          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "LSP: Go to References" })
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "LSP: Go to References" })
           vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Actions" })
           vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "LSP: Rename" })
         end
