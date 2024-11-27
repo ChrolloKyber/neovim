cd00c3129e1e7806c25d995081db8f8b50af03d8
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-27 16:18:58 +0530

    tpl is json now
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/ft.lua
+++ lua/chrollo/ft.lua
@@ -1,15 +1,24 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*",
-  callback = function()
-    if vim.bo.filetype == "tf" then
-      vim.bo.filetype = "terraform"
-    end
-  end,
+	pattern = "*",
+	callback = function()
+		if vim.bo.filetype == "tf" then
+			vim.bo.filetype = "terraform"
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
-  callback = function()
-    vim.bo.filetype = "yaml.docker-compose"
-  end,
+	pattern = "*.tpl",
+	callback = function()
+		if vim.bo.filetype == "smarty" then
+			vim.bo.filetype = "json"
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
+	callback = function()
+		vim.bo.filetype = "yaml.docker-compose"
+	end,
 })
