6c0e257e7a9d28ef6420e87c158e5e671c8c389d
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-28 17:19:50 +0530

    lualine changes
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/qol.lua
+++ lua/chrollo/plugins/qol.lua
@@ -34,10 +34,34 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+      local custom_gruvbox = require 'lualine.themes.gruvbox-material'
+      custom_gruvbox.normal.a.bg = '#32302f'
+      custom_gruvbox.normal.a.fg = '#ddc7a1'
+      custom_gruvbox.normal.c.bg = '#3c3836'
+      custom_gruvbox.insert.a.bg = '#7daea3'
       require("lualine").setup {
         options = {
-          theme = 'gruvbox',
-        }
+          theme = custom_gruvbox,
+          disabled_filetypes = {
+            statusline = { 'neo-tree' },
+            winbar = { 'neo-tree' },
+          },
+        },
+        sections = {
+          lualine_b = { 'branch', 'diff' },
+          lualine_c = {},
+          lualine_x = { 'filename' },
+          lualine_y = {},
+          lualine_z = { { 'filetype', colored = false } },
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = { 'filename' },
+          lualine_y = {},
+          lualine_z = {}
+        },
       }
     end,
   },
