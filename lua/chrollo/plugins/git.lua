dcb2de72f75ba9d6abc0b15c9f04fee9bf884ceb
ChrolloKryber <rishay.raghav00@gmail.com>
2024-12-09 22:17:03 +0530

    gitsigns
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/git.lua
+++ lua/chrollo/plugins/git.lua
@@ -2,17 +2,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup({
-        opts = {
-          signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-          },
-        },
-      })
+      require("gitsigns").setup({})
     end,
   },
 
