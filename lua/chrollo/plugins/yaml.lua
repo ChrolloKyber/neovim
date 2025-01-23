5914cff3170410ade88f4d8f9829acba102c5122
ChrolloKryber <rishay.raghav00@gmail.com>
2025-01-23 11:54:46 +0530

    i hate yaml
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/yaml.lua
+++ lua/chrollo/plugins/yaml.lua
@@ -1,13 +1,29 @@
 return {
-  "someone-stole-my-name/yaml-companion.nvim",
-  ft = { "yaml", "yaml.docker-compose" },
-  dependencies = {
-    { "neovim/nvim-lspconfig" },
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim" },
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    ft = { "yaml", "yaml.docker-compose" },
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("yaml_schema")
+    end,
   },
-  config = function()
-    require("telescope").load_extension("yaml_schema")
-  end,
+  {
+    "cuducos/yaml.nvim",
+    ft = { "yaml", "yaml.docker-compose" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_x = { require("yaml_nvim").get_yaml_key_and_value },
+        }
+      })
+    end
+  }
 }
-
