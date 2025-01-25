64fe097bd9c916ea31ccb1b9183f4f6218c97214
ChrolloKryber <rishay.raghav00@gmail.com>
2025-01-25 14:43:18 +0530

    oil up
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- lua/chrollo/plugins/telescope.lua
+++ lua/chrollo/plugins/telescope.lua
@@ -4,7 +4,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-fzy-native.nvim",
-      "jonarrien/telescope-cmdline.nvim",
     },
     tag = "0.1.8",
     config = function()
@@ -15,30 +14,12 @@ return {
             override_generic_sorter = false,
             override_file_sorter = true,
           },
-          cmdline = {
-            picker = {
-              layout_config = {
-                width = 120,
-                height = 25,
-              },
-            },
-            mappings = {
-              complete = "<TAB>",
-              run_selection = "<C-CR>",
-              run_input = "<CR>",
-            },
-            overseer = {
-              enabled = false,
-            },
-          },
         },
       })
       require("telescope").load_extension("fzy_native")
-      require("telescope").load_extension("cmdline")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "Q", "<CMD>:Telescope cmdline<CR>", { noremap = true, desc = "Cmdline" })
     end,
   },
 }
